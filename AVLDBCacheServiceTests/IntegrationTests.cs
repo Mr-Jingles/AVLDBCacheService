220e6a5be44b1bd096d4acd70d2decc7da864f78
Edward Collins <Coding96@github.com>
2022-10-17 23:23:12 +0100

    move files so that unit tests can be found

---
+++ AVLDBCacheServiceTests/IntegrationTests.cs
@@ -0,0 +1,12 @@
+namespace AVLDBCacheServiceTests
+{
+    //Need to be pass 1st stage dev int before integration tests can be written.
+    public class IntegrationTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
